9432f1283205a69fb924d387d181b0c53e86284e
NetoDevel <josevieira.dev@gmail.com>
2020-05-25 15:43:47 -0300

    Adiciona projeto spring boot

---
+++ src/main/java/com/example/netodevelapi/NetodevelApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.netodevelapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetodevelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetodevelApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/netodevelapi/NetodevelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.netodevelapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetodevelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
